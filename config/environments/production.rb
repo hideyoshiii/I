c74e1fd08526df13b06d2f9081f48be475a000e4
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2017-11-11 11:05:26 +0900

    fix06

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -91,5 +91,5 @@ Rails.application.configure do
 
 
   #本番にあげるときはチェンジ
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'ec2-54-235-254-251.compute-1.amazonaws.com', port: 5432 }
 end
