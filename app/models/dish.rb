493526d898930961781266a21e977bbf24b4e076
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2017-11-11 16:54:16 +0900

    fix23

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -5,7 +5,7 @@ class Dish < ApplicationRecord
   validates :description, presence: true
 
   # photoをattachファイルとする。stylesで画像サイズを定義できる
-  has_attached_file :photo, styles: { large: "970x600", medium: "650x400", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :photo, styles: { large: "700x450", medium: "650x400", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
   # ファイルの拡張子を指定（これがないとエラーが発生する）
   validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
